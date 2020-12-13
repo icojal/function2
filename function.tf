896091e640c4df4f73ce3ff8766b3b44488bd45c
icojal <75904583+icojal@users.noreply.github.com>
2020-12-13 02:15:51 -0500

    Update function.tf

--- function.tf
+++ function.tf
@@ -1,3 +1,27 @@
+##############################################################################
+# IBM Cloud Provider
+##############################################################################
+
+provider ibm {
+  ibmcloud_api_key      = "${var.ibmcloud_apikey}"
+  region                = "${var.ibm_region}"
+  generation            = 1
+  ibmcloud_timeout      = 60
+}
+
+##############################################################################
+
+
+##############################################################################
+# Resource Group
+##############################################################################
+
+data ibm_resource_group resource_group {
+  name = "${var.resource_group}"
+}
+
+##############################################################################
+
 resource "ibm_function_action" "nodehello" {
   name      = "action-name"
   namespace = "function-namespace-name"
