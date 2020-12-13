6e01a076a51f1bdadfdd24c73e762d3dc1e8fb4c
icojal <75904583+icojal@users.noreply.github.com>
2020-12-13 11:25:21 -0500

    Update function.tf

--- function.tf
+++ function.tf
@@ -31,3 +31,31 @@ resource ibm_function_action "nodehello" {
     #code = file("index.js")
   }
 }
+
+
+/*
+resource "ibm_function_namespace" "namespace" {
+  name              = var.function_namespace
+  description       = "Expand 2020"
+  resource_group_id = data.ibm_resource_group.resource_group.id
+}
+resource "ibm_function_package" "package" {
+  name      = var.function_package_name
+  namespace = ibm_function_namespace.namespace.name
+}
+resource "ibm_function_action" "hello_action" {
+  name      = "${var.function_package_name}/hello-${var.environment}"
+  namespace = ibm_function_namespace.namespace.name
+  exec {
+    kind = "nodejs:10"
+    code = file("src/function/index.js")
+  }
+  user_defined_parameters = <<EOF
+        [
+    {
+        "key":"environment",
+        "value":"${var.environment}"
+    }
+        ]
+EOF
+}*/
